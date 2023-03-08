325af859632187e230c2bb1f808148c89b43ad4c
zezoElhossin <cds.zoelfakarMohamed11198@alexu.edu.eg>
2023-03-08 20:59:04 +0200

    installing and loading the required packagess

---
+++ Lab2_Exercies.R
@@ -0,0 +1,11 @@
+#  install the required packages
+install.packages("dplyr", dependencies = TRUE)
+install.packages("ggplot2", dependencies = TRUE)
+install.packages("ggpubr", dependencies = TRUE)
+install.packages("gcookbook", dependencies = TRUE)
+
+# load the required packages
+library(dplyr)
+library(ggplot2)
+library(ggpubr)
+library(gcookbook)
