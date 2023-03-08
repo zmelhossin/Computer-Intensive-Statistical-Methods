096b1b30457bf93a6b2dd8f2a9462eca4d80101b
zezoElhossin <cds.zoelfakarMohamed11198@alexu.edu.eg>
2023-03-08 21:24:37 +0200

    End of lab2 Exercies

--- Lab2_Exercies.R
+++ Lab2_Exercies.R
@@ -9,3 +9,17 @@ library(dplyr)
 library(ggplot2)
 library(ggpubr)
 library(gcookbook)
+
+#Exercises
+#First find the formula for the BMI and calculate it for the data set (heightweight)
+data(heightweight)
+heightweight['heightM'] = heightweight$heightIn / 39.37 # to convert the height from inch to meter.
+heightweight['BMI'] = heightweight$weightLb / (heightweight$heightM)^2 # adding BMI column to the dataset >>>> BMI = w / h^2 .
+
+#Draw the ecdf for the BMI 
+ggplot(heightweight, aes(x = BMI)) + stat_ecdf()
+
+#According to the following BMI category and the drawn ecdf, can you determine the percentage of people 
+#that are Underweight and Overweight. Underweight = < 18.5   Overweight = 25-29.9
+sum(heightweight$BMI < 18.5) #Underweight percentages = Zero.
+nrow(heightweight[heightweight$BMI >= 25 & heightweight$BMI <= 29.9, ])# Overwight percentages = 1/236
