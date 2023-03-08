4a448de71861d95b6e1d596d67553a405bd212df
zezoElhossin <cds.zoelfakarMohamed11198@alexu.edu.eg>
2023-03-08 14:48:09 +0200

    plotting the price column and finding summary stats about:

--- Lab1_Exercies.R
+++ Lab1_Exercies.R
@@ -7,3 +7,17 @@ library(ggplot2)
 
 # load ames data:
 data(ames)
+
+# plotting the price column and finding summary stats about:
+ggplot(data = ames, aes(x = price)) + geom_histogram(binwidth = 20000) # the distribution takes right skew shape.
+ames %>%
+  summarise(mu = mean(price), pop_median = median(price), sigma = sd(price),
+            pop_iqr = IQR(price), pop_min = min(price),
+            pop_max = max(price), pop_q1 = quantile(price, 0.25),
+            pop_q3 = quantile(price, 0.75)) #mean of pop price = 180796.1
+
+
+  
+  
+  
+  
