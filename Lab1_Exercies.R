c8b6fa8d59c79c0fc950213ef34490b1f46d2616
zezoElhossin <cds.zoelfakarMohamed11198@alexu.edu.eg>
2023-03-08 14:55:00 +0200

    E1 solved

--- Lab1_Exercies.R
+++ Lab1_Exercies.R
@@ -15,8 +15,31 @@ ames %>%
             pop_iqr = IQR(price), pop_min = min(price),
             pop_max = max(price), pop_q1 = quantile(price, 0.25),
             pop_q3 = quantile(price, 0.75)) #mean of pop price = 180796.1
+#-----------------------------------------------------
+# E1: Take a random sample of size 50 from price. Using this sample, what is your best point estimate of the 
+#population mean?
+sampl <- ames %>%
+  sample_n(size = 50)
+mean(sampl$price) # sample size 50 mean = 185798.7 "best point estimate for pop mean"
 
 
+#E2: Since you have access to the population, simulate the sampling distribution for 𝑥̅𝑝𝑟𝑖𝑐𝑒 by taking 5000 
+#samples from the population of size 50 and computing 5000 sample means. Store these means in a vector 
+#called sample_means50. Plot the data, then describe the shape of this sampling distribution. Based on this 
+#sampling distribution, what would you guess the mean home price of the population to be?
+
+
+
+
+
+
+
+
+  
+
+  
+  
+  
   
   
   
